57e3ad9bdfa1990b112ac695b378d15ac2739ce0
José Ruas <joseluis.ruas@gmail.com>
2020-02-09 22:10:47 -0300

    add mail libs using html templates

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '2fced473467138',
+    pass: 'd8e1a6af10c8fb',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
